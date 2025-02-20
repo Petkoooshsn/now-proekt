e78fc08a1d022faf6d3cc125babbd0841105d574
Петко Шопов <petkoshopov41@gmail.com>
2025-02-20 08:46:13 +0200

    webapp4

--- WebApplication4/Controllers/HomeController.cs
+++ WebApplication4/Controllers/HomeController.cs
@@ -1,49 +1,23 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using System.Diagnostics;
-using WebApplication4.Models;
+using System;
 
-namespace WebApplication4.Controllers
+namespace YourNamespace.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
+        private const string CookieLastVisit = "LastVisit";
 
         public IActionResult Index()
         {
             string lastVisitMessage = "Това е първото ви посещение!";
-            string cookieName = "LastVisit";
 
-            // Проверка дали има бисквитка с последното посещение
-            if (Request.Cookies.TryGetValue(cookieName, out string lastVisit))
+            if (Request.Cookies.TryGetValue(CookieLastVisit, out string lastVisit))
             {
                 lastVisitMessage = $"Последно посещение: {lastVisit}";
             }
 
-            // Запис на текущата дата и час в бисквитка
-            CookieOptions options = new CookieOptions
-            {
-                Expires = DateTime.Now.AddYears(1) // Бисквитката ще е валидна 1 година
-            };
-            Response.Cookies.Append(cookieName, DateTime.Now.ToString("dd.MM.yyyy HH:mm:ss"), options);
-
             ViewBag.LastVisit = lastVisitMessage;
             return View();
-        }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
 }
